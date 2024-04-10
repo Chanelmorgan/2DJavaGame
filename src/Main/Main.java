20b0ecc5ff87a4a3ef6413a60ac4b530864958b6
Chanel Morgan <chanelmorgan05@icloud.com>
2024-04-10 08:41:52 +0100

    Creating the GamePanel class and adding it to the window

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -11,6 +11,11 @@ public class Main {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("2D Adventure");
+
+        // Adding the game panel
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
         window.setLocationRelativeTo(null); // opens window at centre of screen
         window.setVisible(true);
     }
